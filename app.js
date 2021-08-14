c1fb90207bfdd7009c9b50ec40632c782837ed5d
kellydhunter1 <kellydhunter1@gmail.com>
2021-08-14 19:12:51 -0400

    stylesheet copied and HTML file writes to dist folder

--- app.js
+++ app.js
@@ -139,11 +139,22 @@ promptUser = () => {
   .then(promptProject)
   .then(portfolioData => {
       const pageHTML = generatePage(portfolioData);
-    fs.writeFile('./index.html', pageHTML, err => {
-        if (err) throw new Error(err);
-      console.log('Page created! Check out index.html in this directory to see it!');
+    fs.writeFile('./dist/index.html', pageHTML, err => {
+        if (err) {
+          console.log(err);
+          return;
+        }
+        console.log('Page created! Check out index.html in this directory to see it!');
+
+      fs.copyFile('./src/style.css', './dist/style.css', err => {
+        if (err) {
+          console.log(err);
+          return;
+        }
+        console.log("Stylesheet copied successfully!")
       });
     });
+  });
 
 // // variable for this file
 // const pageHTML = generatePage(name, github);
