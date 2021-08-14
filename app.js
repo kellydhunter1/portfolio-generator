f096f815b133b72831f1677b1fccab8f56abbc49
kellydhunter1 <kellydhunter1@gmail.com>
2021-08-14 12:02:45 -0400

    refactored code to separate template file and connected modules

--- app.js
+++ app.js
@@ -1,16 +1,28 @@
+// allows js to create file using local file system
+const fs = require('fs');
+// allows other other files to connect to functions/objects in this file
+const generatePage = require('./src/page-template.js');
+
+
+// variable for this file
 const profileDataArgs = process.argv.slice(2, process.argv.length);
-
-const printProfileData = profileDataArr => {
-    // This...
-    for (let i = 0; i < profileDataArr.length; i += 1) {
-      console.log(profileDataArr[i]);
-    }
-  
-    console.log('================');
-  
-    // Is the same as this...
-    profileDataArr.forEach(profileItem => console.log(profileItem));
-  };
+const [name, github] = profileDataArgs;
 
 
-printProfileData(profileDataArgs);
+// write user input to an HTML file
+fs.writeFile('./index.html', generatePage(name, github), err => {
+  if (err) throw new Error(err);
+
+  console.log('Portfolio complete! Check out index.html to see the output!');
+});
+
+
+// PRINT INPUT
+// generatePage = (name, github) => {
+//   return `
+//   Name: ${name}
+//   GitHub: ${github}
+//   `;
+// };
+// console.log(name, github);
+// console.log(generatePage(name, github));
