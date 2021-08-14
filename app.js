cde187beb47b3816db5b566e00273f2688cd345f
kellydhunter1 <kellydhunter1@gmail.com>
2021-08-14 12:47:14 -0400

    added prompt with inquirer

--- app.js
+++ app.js
@@ -1,28 +1,38 @@
-// allows js to create file using local file system
-const fs = require('fs');
-// allows other other files to connect to functions/objects in this file
-const generatePage = require('./src/page-template.js');
+// // npm packages
+const inquirer = require('inquirer');
+  // // npm modules
+// const fs = require('fs');
+
+// // project modules
+// const generatePage = require('./src/page-template.js');
+
+inquirer.prompt([
+    {
+      type: 'input',
+      name: 'name',
+      message: 'What is your name?'
+    }
+  ])
+  .then(answers => console.log(answers));
 
 
-// variable for this file
-const profileDataArgs = process.argv.slice(2, process.argv.length);
-const [name, github] = profileDataArgs;
+// // variable for this file
+// const pageHTML = generatePage(name, github);
+
+// // write user input to an HTML file
+// fs.writeFile('./index.html', generatePage(name, github), err => {
+//   if (err) throw new Error(err);
+
+//   console.log('Portfolio complete! Check out index.html to see the output!');
+// });
 
 
-// write user input to an HTML file
-fs.writeFile('./index.html', generatePage(name, github), err => {
-  if (err) throw new Error(err);
-
-  console.log('Portfolio complete! Check out index.html to see the output!');
-});
-
-
-// PRINT INPUT
-// generatePage = (name, github) => {
-//   return `
-//   Name: ${name}
-//   GitHub: ${github}
-//   `;
-// };
-// console.log(name, github);
-// console.log(generatePage(name, github));
+// // PRINT INPUT
+// // generatePage = (name, github) => {
+// //   return `
+// //   Name: ${name}
+// //   GitHub: ${github}
+// //   `;
+// // };
+// // console.log(name, github);
+// // console.log(generatePage(name, github));
